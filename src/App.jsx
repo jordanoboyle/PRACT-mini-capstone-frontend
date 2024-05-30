03c9bbb0fbcfd8f4c43345459095a591e387f16d
jordanoboyle <jmcmcode@gmail.com>
2024-05-30 10:50:20 -0600

    name, image_url, description, price added to products index

--- src/App.jsx
+++ src/App.jsx
@@ -29,9 +29,33 @@ function ProductsNew() {
   )
 }
 
-function ProductsIndex() {}
+function ProductsIndex() {
+  return (
+    <div id="products-index">
+      <h1> Our Curated Collection </h1>
+      <div className= 'Products'>
+        <p>Product Name: Halo Infinite Shirt</p>
+        <img src="https://m.media-amazon.com/images/I/81r6IF9GNNL._AC_UF1000,1000_QL80_.jpg" />
+        <p>
+          Description: Here is some glorious information about Halo 6, otherwise Halo Infinite.
+          Good game with some much needed patching. This is a late review but, still truthful to this day. 
+          Now go get them Covenant.
+        </p>
+        
+      </div>
 
-function Footer() {}
+    </div>
+  )
+}
+
+function Footer() {
+  return (
+  <footer>
+    <p>Copyright Year of the APE</p>
+    <p>Site by Jordan O'Boyle</p>
+  </footer>
+  )
+}
 
 function Content() {
   return (
