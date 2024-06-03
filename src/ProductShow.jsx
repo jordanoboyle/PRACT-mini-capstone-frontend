c8a0ad2149578d4c63da8ceaab1565d9d0a4073e
jordanoboyle <jmcmcode@gmail.com>
2024-06-03 11:28:18 -0600

    delete form

--- src/ProductShow.jsx
+++ src/ProductShow.jsx
@@ -1,9 +1,12 @@
-
+import axios from "axios"
 
 export function ProductShow(props) {
 
   const deleteProduct = () => {
-    console.log("deleting the recipe")
+    console.log("deleting the recipe");
+    axios.delete(`http://localhost:3000/products/${props.product.id}.json`).then(response => {
+      console.log(response.data)
+    })
   }
   
   return (
@@ -15,7 +18,22 @@ export function ProductShow(props) {
       <p>Number Remaining: {props.product.inventory} </p>
       <p>Supplier(must be 1-4): {props.product.supplier.name}</p>
       <form>
-        <div>SPACE FOR UPDATE</div>
+        <div>UPDATE PRODUCT INFORMATION BELOW</div>
+        <div>
+          Name: <input type="text" name="name" defaultValue={props.product.name} />
+        </div>
+        <div>
+          Description: <input type="text" name="description" defaultValue={props.product.description} />
+        </div>
+        <div>
+          Price: <input type="text" name="price" defaultValue={props.product.price} />
+        </div>
+        <div>
+          Inventory: <input type="text" name="inventory" defaultValue={props.product.inventory} />
+        </div>
+        <div>
+          Supplier: <input type="text" name="supplier_id" defaultValue={props.product.supplier_id} />
+        </div>
       </form>
       <br />
       <br />
