16b291164f881d865a3b23b2e527ba795d2ea086
jordanoboyle <jmcmcode@gmail.com>
2024-06-28 12:42:47 -0600

    search dynamic on title, auto complete hardcode

--- src/ProductsIndex.jsx
+++ src/ProductsIndex.jsx
@@ -7,9 +7,16 @@ export function ProductsIndex(props) {
   return (
     <div>
       <h1> Our Curated Collection </h1>
-      <p>Search: <input  type="text" placeholder="Search by title..."/></p>
+      <p>Search: <input  
+      type="text" 
+      placeholder="Search by title..." 
+      value={searchTerm} onChange={event => setSearchTerm(event.target.value)} 
+      list="product names"/></p>
+      <datalist id="product names">
+        <option>hoody</option>
+      </datalist>
       <div className="row">
-        {props.products.map(product => (
+        {props.products.filter(product => product.name.toLowerCase().includes(searchTerm.toLowerCase())).map(product => (
         <div className="col-sm-3 mb-3 mb-sm-0" key={product.id}>
           <div className="card">
             <div className="card-body">
